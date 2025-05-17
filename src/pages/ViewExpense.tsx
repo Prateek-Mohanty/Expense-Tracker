3e7cbdfbeb4f1b530dbe2380ede66be238b7801a
Prateek Mohanty <mohantyprateekkumar@gmail.com>
2025-05-17 21:53:14 +0530

    minor changes before backend integration

--- src/pages/ViewExpense.tsx
+++ src/pages/ViewExpense.tsx
@@ -1,30 +1,35 @@
-import { Expense } from '../common/Expense'
+import { Expense } from '../common/Expense';
 import { FC } from 'react';
 import Navbar from "../components/Navbar";
-import '../App.css'
+import '../App.css';
 
-export interface ViewExpensesProps{
-    expenses: Expense[]
+export interface ViewExpensesProps {
+  expenses: Expense[];
 }
 
 const ViewExpense: FC<ViewExpensesProps> = ({ expenses }) => {
   return (
     <div className="container">
-    <h1>Expenses</h1>
-    <div>
-        {expenses.length === 0 ?(
-            <p>No expense added yet</p>
-        ):(
-            <ul>
-                {expenses.map((expense)=>{
-                    return(<li key={expense.id}>{`${expense.expenseName}-₹${expense.amount}:${expense.date}`}</li>)
-                })}
-            </ul>
+      <h1>Expenses</h1>
+      <div>
+        {expenses.length === 0 ? (
+          <p>No expense added yet</p>
+        ) : (
+          <ul className="expense-list">
+            {expenses.map((expense) => {
+              const formattedDate = new Date(expense.date).toLocaleDateString();
+              return (
+                <li key={expense.id} className="expense-item">
+                  <strong>{expense.expenseName}</strong> - ₹{expense.amount} <span className="date">({formattedDate})</span>
+                </li>
+              );
+            })}
+          </ul>
         )}
+      </div>
+      <Navbar />
     </div>
-    <Navbar/>
-    </div>
-  )
-}
+  );
+};
 
-export default ViewExpense
+export default ViewExpense;
