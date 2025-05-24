3fe69636625014126845324ea0d13c58cf0bb9fa
Prateek Mohanty <mohantyprateekkumar@gmail.com>
2025-05-24 11:55:16 +0530

    formatting change

--- src/pages/AddExpense.tsx
+++ src/pages/AddExpense.tsx
@@ -9,54 +9,54 @@ interface AddExpenseProps {
 }
 
 function AddExpense({addExpense}: AddExpenseProps) {
-    const [formData, setFormData] = useState({
-        expenseName: '',
-        amount: '',
-        date: ''
-    });
+  const [formData, setFormData] = useState({
+      expenseName: '',
+      amount: '',
+      date: ''
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({
-          ...prev,
-          [name]: value
-        }));
-      };
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (formData.expenseName && formData.amount && formData.date) {
-          addExpense({
-            ...formData,
-            amount: parseFloat(formData.amount),
-            id: Date.now(),
-          });
-          setFormData({ expenseName: '', amount: '', date: '' }); 
-        } else {
-          alert('Please fill in all fields.');
-        }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value
+      }));
     };
+
+  const handleSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      if (formData.expenseName && formData.amount && formData.date) {
+        addExpense({
+          ...formData,
+          amount: parseFloat(formData.amount),
+          id: Date.now(),
+        });
+        setFormData({ expenseName: '', amount: '', date: '' }); 
+      } else {
+        alert('Please fill in all fields.');
+      }
+  };
   return (
     <div className="container">
     <Navbar/>
     <h1>Add Expense</h1>
     <form>
-        {inputConfig.map((ele)=>{
-            return(
-              <div key={ele.id}>
-                <label htmlFor={ele.labelFor}>{ele.label}</label>
-                <input 
-                  type={ele.type}
-                  name={ele.name}
-                  id={ele.id}
-                  value={formData[ele.name as keyof typeof formData]} 
-                  onChange={handleChange} 
-                  required
-                />
-              </div>
-            )
-        })}
-        <button type="submit" onClick={handleSubmit}>Add Expense</button>
+      {inputConfig.map((ele)=>{
+        return(
+          <div key={ele.id}>
+            <label htmlFor={ele.labelFor}>{ele.label}</label>
+            <input 
+              type={ele.type}
+              name={ele.name}
+              id={ele.id}
+              value={formData[ele.name as keyof typeof formData]} 
+              onChange={handleChange} 
+              required
+            />
+          </div>
+        )
+      })}
+      <button type="submit" onClick={handleSubmit}>Add Expense</button>
     </form>
     </div>
   )
